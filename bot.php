9d9f67b1c504ba4cd00c176bdf457ed463e4b066
asri-utcc <asri.utcc@gmail.com>
2016-12-05 19:16:41 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -47,8 +47,8 @@ if (!is_null($events['events'])) {
 function text($text) {
     $myfile = fopen("listword.txt", "r") or die("Unable to open file!");
 	//echo fread($myfile,filesize("webdictionary.txt"));
-	$zTmp = fgets($myfile);	
-	/*while (($zTmp = fgets($myfile))) !== false) {
+	//$zTmp = fgets($myfile);	
+	while (($zTmp = fgets($myfile))) !== false) {
 		$first = strtok($zTmp, '.');
 		if (strpos($text, $first) !== false) {
 			$z = substr($zTmp,strrpos($zTmp, '.') + 1);
@@ -56,11 +56,11 @@ function text($text) {
 		}else {
 			$z = "อาอิชสวย";
 		}
-	}*/
+	}
 	
 	
 	fclose($myfile);
-    return $zTmp;
+    return $z;
 	}
 echo "OK";
 ?>
