787c821501525f5509b11ed81bddef9c313a7bb8
asri-utcc <asri.utcc@gmail.com>
2016-12-05 17:02:18 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -43,6 +43,7 @@ function text($text) {
     $myfile = fopen("listword.txt", "r") or die("Unable to open file!");
 	//echo fread($myfile,filesize("webdictionary.txt"));
 	$z = fgets($myfile);
+	$z = fgets($myfile);
 	fclose($myfile);
 	
 	/*if (strpos($text, 'รัยมี') !== false) {
