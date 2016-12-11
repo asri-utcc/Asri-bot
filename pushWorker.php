dd1c20af93cd83dbd345b5a1632e4b75208ed079
asri-utcc <asri.utcc@gmail.com>
2016-12-11 23:12:21 +0700

    Add files via upload

--- pushWorker.php
+++ pushWorker.php
@@ -6,15 +6,17 @@ function get_url_contents($url){
 		curl_setopt ($crl, CURLOPT_PORT,'3333');
         curl_setopt ($crl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt ($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
-        $ret = curl_exec($crl);
+        
+		if(curl_error($crl)){
+			$ret = 'error:' . curl_error($crl);
+		}else {
+			$ret = curl_exec($crl);
+		}
         curl_close($crl);
         return $ret;
 }
 
 $html = get_url_contents("http://54d69050d538f.sn.mynetname.net");
-if ($html !== null){
-	echo $html;
-	} else {
-		echo 'cannot check';
-	}
+echo $html;
+
 ?>
