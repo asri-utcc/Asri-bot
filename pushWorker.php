bfa417436f3e9c4d183c378d4718af711058cfac
asri-utcc <asri.utcc@gmail.com>
2016-12-12 23:29:15 +0700

    Add files via upload

--- pushWorker.php
+++ pushWorker.php
@@ -1,9 +1,9 @@
 <?php
-function get_url_contents($url){
+function get_url_contents($url,$port){
         $crl = curl_init();
         $timeout = 10;
         curl_setopt ($crl, CURLOPT_URL,$url);
-		curl_setopt ($crl, CURLOPT_PORT,'3333');
+		curl_setopt ($crl, CURLOPT_PORT,$port);
         curl_setopt ($crl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt ($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
         
@@ -15,9 +15,33 @@ function get_url_contents($url){
         curl_close($crl);
         return $ret;
 }
+$x1=0;
+while($x1 <= 2) {
+	$html = get_url_contents("http://5d69050d538f.sn.mynetname.net",'3333');
+	$z1 = substr($html,34,6);
+	$x1++;
+	if ($z1 !== 'result'){
+		sleep(5);
+	}	else {
+		$x1 = 10;
+	}
+	
+}
 
-$html = get_url_contents("http://5d69050d538f.sn.mynetname.net");
-$z = substr($html,34,6);
+$x2=0;
+while($x2 <= 2) {
+	$html = get_url_contents("http://5d69050d538f.sn.mynetname.net",'3334');
+	$z2 = substr($html,34,6);
+	$x2++;
+	if ($z2 !== 'result'){
+		sleep(5);
+	}	else {
+		$x2 = 10;
+	}
+	
+}
+
+$z='rig1=' . x1 . 'rig2=' . x2;
 
 $access_token = 'pTEHeInoIt5on143F+kYg6D//GGUbrDcPTFtrXQGFLCVFPa3OZbjVBkug2cTevT0EkeCF68zeILcBXw28aiucQco/qtCf6HgfgxIuJX0Lm9TSV8WV6iQdGa1KXFuOQvQ0dbT1tinCAqfC6B8MrGyxgdB04t89/1O/w1cDnyilFU=';
 $userID = 'U57bbd5f3aa4dd14369a6969c281e6652';
