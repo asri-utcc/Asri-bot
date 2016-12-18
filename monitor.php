6df9add9c3a1f1826aaa19200b2ef624be6f6438
asri-utcc <asri.utcc@gmail.com>
2016-12-18 23:52:20 +0700

    Add files via upload

--- monitor.php
+++ monitor.php
@@ -53,9 +53,10 @@ for ($i = 0; $i < $linecount; $i++){
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		$result = curl_exec($ch);
 		curl_close($ch);
-		$file1 = after(',',$file1);
+		
 		echo $result;
 	}
+	$file1 = after(',',$file1);
 }
 
 
