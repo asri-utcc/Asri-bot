18e1422d07f3005d7bef0f36ad179b10ae9d59e2
asri-utcc <asri.utcc@gmail.com>
2017-01-02 14:38:57 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -68,7 +68,12 @@ function text($text) {
 					$z1 = $z2*between ('last":"', '",', $zeth1);
 					//$z1 = number_format((float)$z1, 3, '.', '');
 			}
-			$z = strtoupper($coin) . " ตอนนี้ราคา $" . $z1 . PHP_EOL . "หรือ " . $z2 . " btc ค่ะ" . PHP_EOL . "หรือประมาน " . number_format((float)($z1 * 35.5), 2, '.', '') . " บาทค่ะ";
+			if($z1>15){
+				$z1 = number_format((float)($z1 * 35.5), 2, '.', '')
+			} else (
+				$z1 = number_format((float)($z1 * 35.5), 4, '.', '')
+			)
+			$z = strtoupper($coin) . " ตอนนี้ราคา $" . $z1 . PHP_EOL . "หรือ " . $z2 . " btc ค่ะ" . PHP_EOL . "หรือประมาน " . $z1 . " บาทค่ะ";
 			
 			} else {
 				$z = "วิธีการใช้คำสั้ง coin คือ" . PHP_EOL . "coin <CryptoCurrency: btc eth etc xmr>";
